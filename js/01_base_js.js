86cd2c358ae349bf26a5ac519a150a05a61d8ba6
Vincent Lefebvre <58303000+vince93310@users.noreply.github.com>
2019-12-18 17:08:56 +0100

    bonjour

---
+++ js/01_base_js.js
@@ -0,0 +1,18 @@
+//*******Syntaxe de base******//
+
+// 1.Je vérifie toujours que ma page JS et mon fichier HTML sont connectés :
+// alert('Super, tu es arrivée sur mon site')
+// prompt('Question : on est quel jour?')
+// deux slashes pour un commentaire sur une ligne
+
+/*
+(shift + alt + a)
+ici un commentaire sur plusieurs ligne
+*/
+
+/*
+2 manières d'afficher une boite de dialogue : 
+    =>alert('Super, tu es arrivée sur mon site')
+    =>prompt('Question : on est quel jour?'); avec la fonction prompt je dialogue directement avec l'utilisateur
+*/
+/*  */
---
+++ js/bonjour.js
@@ -0,0 +1,5 @@
+// alert('js ok');
+// Boite de dial
+var prenom = prompt('Quel est ton prénom ?')
+var nom = prompt('Quel est ton nom?')
+alert('Bonjour' + "" + prenom + "" + nom)
